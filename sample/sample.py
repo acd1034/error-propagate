25b84f4698ce9d75a90221ee1aa801a29b2ab9e0
acd1034 <48613285+acd1034@users.noreply.github.com>
2024-03-01 12:08:51 +0900

    Refactor propagate

--- sample/sample.py
+++ sample/sample.py
@@ -27,59 +27,61 @@ def _unique_id() -> int:
 _unique_id.count = 0
 
 
+def _rewrite_func_body(func_body: list[ast.stmt], ErrorType: type):
+    pos: int = 0
+
+    @dataclass
+    class ReplaceName(ast.NodeTransformer):
+        name_to_replace: str
+        new_node: ast.AST
+
+        def visit_Name(self, node):
+            if node.id == self.name_to_replace:
+                return self.new_node
+            return node
+
+    class ReplaceFuncCall(ast.NodeTransformer):
+        def visit_Call(self, node):
+            if (
+                isinstance(node.func, ast.Name)
+                and node.func.id == "TRY"
+                and len(node.args) == 1
+            ):
+                id = _unique_id()
+                tmpvar = f"_tmpvar{id}_"
+                tmpexpr = f"_tmpexpr{id}_"
+
+                return_stmts = ast.parse(
+                    f"{tmpvar} = {tmpexpr}\n"
+                    f"if isinstance({tmpvar}, {ErrorType.__name__}):\n"
+                    f"    return {tmpvar}\n"
+                )
+                return_stmts = ReplaceName(tmpexpr, node.args[0]).visit(
+                    return_stmts
+                )
+                # insert return_stmts into func_body
+                nonlocal pos
+                func_body[pos:pos] = return_stmts.body
+                pos += len(return_stmts.body)
+
+                name_expr = ast.parse(f"{tmpvar}")
+                return name_expr.body[0].value
+            return node
+
+    while pos < len(func_body):
+        func_body[pos] = ReplaceFuncCall().visit(func_body[pos])
+        pos += 1
+
+
 def propagate(ErrorType: type):
     def propagate_wrapper(func):
         source = _getsource(func)
         tree = ast.parse(source)
 
-        func_body = tree.body[0].body
-        pos: int = 0
-
-        @dataclass
-        class ReplaceName(ast.NodeTransformer):
-            name_to_replace: str
-            new_node: ast.AST
-
-            def visit_Name(self, node):
-                if node.id == self.name_to_replace:
-                    return self.new_node
-                return node
-
-        def insert_return_stmts_into_func_body(node):
-            id = _unique_id()
-            tmpvar = f"_tmpvar{id}_"
-            tmpexpr = f"_tmpexpr{id}_"
-
-            return_stmts = ast.parse(
-                f"{tmpvar} = {tmpexpr}\n"
-                f"if isinstance({tmpvar}, {ErrorType.__name__}):\n"
-                f"    return {tmpvar}\n"
-            )
-            return_stmts = ReplaceName(tmpexpr, node.args[0]).visit(
-                return_stmts
-            )
-            # insert return_stmts into func_body
-            nonlocal pos
-            func_body[pos:pos] = return_stmts.body
-            pos += len(return_stmts.body)
-
-            name_expr = ast.parse(f"{tmpvar}")
-            return name_expr.body[0].value
-
-        class ReplaceFuncCall(ast.NodeTransformer):
-            def visit_Call(self, node):
-                if (
-                    isinstance(node.func, ast.Name)
-                    and node.func.id == "TRY"
-                    and len(node.args) == 1
-                ):
-                    return insert_return_stmts_into_func_body(node)
-                return node
-
-        while pos < len(func_body):
-            func_body[pos] = ReplaceFuncCall().visit(func_body[pos])
-            pos += 1
-
+        assert len(tree.body) == 1
+        assert isinstance(tree.body[0], ast.FunctionDef)
+        func_def = tree.body[0]
+        _rewrite_func_body(func_def.body, ErrorType)
         new_tree = ast.fix_missing_locations(tree)
 
         # get the compiled function
